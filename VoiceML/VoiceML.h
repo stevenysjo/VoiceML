6cbecbaa46dc66dba48e84492ba9157483f4e4b4
stevenysjo <steven@SJ-MBP.local>
2018-07-20 13:56:39 +0700

    initial commit

---
+++ VoiceML/VoiceML.h
@@ -0,0 +1,19 @@
+//
+//  VoiceML.h
+//  VoiceML
+//
+//  Created by Steven on 20/7/18.
+//  Copyright © 2018 SJ Dev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VoiceML.
+FOUNDATION_EXPORT double VoiceMLVersionNumber;
+
+//! Project version string for VoiceML.
+FOUNDATION_EXPORT const unsigned char VoiceMLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VoiceML/PublicHeader.h>
+
+
